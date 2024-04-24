27a29898c432fd372779706b819e16ee84bffb5f
Serhii <serhiiromanchuk@gmail.com>
2024-04-24 17:55:12 +0300

    Add App Theme

--- app/src/main/java/com/example/composecampgroup4/presentation/MainActivity.kt
+++ app/src/main/java/com/example/composecampgroup4/presentation/MainActivity.kt
@@ -3,18 +3,25 @@ package com.example.composecampgroup4.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat
 import com.example.composecampgroup4.presentation.theme.ComposeCampGroup4Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setupFullScreen()
         setContent {
             ComposeCampGroup4Theme {
                 // A surface container using the 'background' color from the theme
@@ -22,25 +29,13 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeCampGroup4Theme {
-        Greeting("Android")
+    private fun setupFullScreen() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
     }
 }
