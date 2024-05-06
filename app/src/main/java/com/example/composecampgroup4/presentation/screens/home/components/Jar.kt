222dc1d6174287b15491b9905dd44d05f0fb4b7c
kozub <kozubzkiy@gmail.com>
2024-05-06 17:49:30 +0300

    imports fix

--- app/src/main/java/com/example/composecampgroup4/presentation/screens/home/components/Jar.kt
+++ app/src/main/java/com/example/composecampgroup4/presentation/screens/home/components/Jar.kt
@@ -18,7 +18,6 @@ import androidx.compose.material3.VerticalDivider
 import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
