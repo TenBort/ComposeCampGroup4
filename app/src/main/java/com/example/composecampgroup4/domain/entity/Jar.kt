97b9db3b5f8982a1a4acf2806b033ff5832d88c4
Serhii <serhiiromanchuk@gmail.com>
2024-05-09 13:55:46 +0300

    Fixed UI on Home and Details screens

--- app/src/main/java/com/example/composecampgroup4/domain/entity/Jar.kt
+++ app/src/main/java/com/example/composecampgroup4/domain/entity/Jar.kt
@@ -12,7 +12,5 @@ data class Jar (
     val description: String = "",
     val closed: Boolean = false,
     val userComment: String = "",
-  
-    var isExpanded: Boolean = false,
     var isFavourite: Boolean = false
 )
