ef1a510c88e4934b6dfdb98f67b3892afa709a3b
Serhii <serhiiromanchuk@gmail.com>
2024-05-11 17:16:12 +0300

    Fixed bug with commentTextField in AddJarScreen

--- app/src/main/java/com/example/composecampgroup4/presentation/screens/add_jar/components/AddJarTextFields.kt
+++ app/src/main/java/com/example/composecampgroup4/presentation/screens/add_jar/components/AddJarTextFields.kt
@@ -9,7 +9,7 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -35,7 +35,9 @@ fun ColumnScope.AddJarTextFields(
     val focusRequester = remember { FocusRequester() }
     val focusManager = LocalFocusManager.current
 
-    SideEffect { focusRequester.requestFocus() }
+    LaunchedEffect(uiState.link != "") {
+        focusRequester.requestFocus()
+    }
 
     Text(
         modifier = Modifier.align(Alignment.Start),
